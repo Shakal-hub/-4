0d6d630ae7174b24cf2edbe17417b686e6b8d002
Shakal-hub <lost.connect@mail.ru>
2022-12-02 21:12:54 +0700

    insert_b

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int b = new int();
             Console.WriteLine("Type:\n\ta - insert value of a\n\tb - insert value of b\n\t+ - perform summation\n\t- - perform substraction\n\t* - perform multiplication\n\t/ - perform division");
+            string command = Console.ReadLine();
+            if (command != null)
+            {
+                if (command == "b")
+                {
+                    Console.WriteLine("Set value:\n");
+                    b = Int32.Parse(Console.ReadLine());
+                }
+            }
         }
     }
 }
